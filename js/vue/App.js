39266f4f5e6370bdabadd1e5818243b3e9d8914f
wlodi <wlodek.grzegorz@gmail.com>
2018-09-28 19:57:45 +0200

    Add correlation to 'simple filter for spreadsheet in each day and global'. Add formatting.

--- js/vue/App.js
+++ js/vue/App.js
@@ -6,49 +6,67 @@ class App {
     
     _renderView() {
       
-      new Vue({
-          el: '#app',
-          data: {
-              sheetsData: null
-          },
-      
-          mounted() {
-              const dataProvider = new GApiSheetProvider(gapi);
-              dataProvider.loadData(SPREADSHEETS_TO_LOAD).then( () => {
-                  this.sheetsData = dataProvider.dataSheetsGroupByDates();
-              });
-          },
-          
-          methods: {
-              filterByDayAndSpreadsheetName: function (day, spreadsheetName) {
-                this.sheetsData.forEach( (item) => {
-                    if(item.day !== day) {
-                        return;
-                    }
-                    
-                    item.activities.forEach( (activity) => {
-                        if(activity.spreadsheetName === spreadsheetName) {
-                            activity.isVisible = !activity.isVisible;
-                        }
-                    });
+        new Vue({
+            el: '#app',
+            data: {
+                sheetsData: null,
+                
+                visibleSpreadsheets: new Array(),
+                visibleSpreadsheetsPerDay: new Array(),
+            },
+
+            created() {
+                SPREADSHEETS_TO_LOAD.forEach( (spreadsheetName) => {
+                    this.visibleSpreadsheets[spreadsheetName] = true;
+                    this.visibleSpreadsheetsPerDay[spreadsheetName] = new Array();
                 });
-              },
+            },
+          
+            mounted() {
+                const dataProvider = new GApiSheetProvider(gapi);
+                dataProvider.loadData(SPREADSHEETS_RANGE_TO_LOAD).then( () => {
+                    this.sheetsData = dataProvider.dataSheetsGroupByDates();
+                    
+                    this.sheetsData.forEach( (item) => {
+                        SPREADSHEETS_TO_LOAD.forEach( (spreadsheetName) => {
+                            this.visibleSpreadsheetsPerDay[spreadsheetName][item.day] = true;
+                        });
+                    } );
+                    
+                });
+            },
+          
+            methods: {
+                filterByDayAndSpreadsheetName: function (day, spreadsheetName) {
+                    let allHasTheSameVisibleValue = true;
+                    this.sheetsData.forEach( (item) => {
+                        item.activities.forEach( (activity) => {
+                            if(activity.spreadsheetName === spreadsheetName) {
+                                if(item.day === day) {
+                                    activity.isVisible = !activity.isVisible;
+                                    this.visibleSpreadsheets[spreadsheetName] = activity.isVisible;
+                                }
+                                if(allHasTheSameVisibleValue !== this.visibleSpreadsheets[spreadsheetName]) {
+                                    this.visibleSpreadsheets[spreadsheetName] = false;
+                                }
+                            }
+                        });
+                    });
+                  },
               
-              filterBySpreadsheetName: function (spreadsheetName) {
-                  this.sheetsData.forEach( (item) => {
-                      item.activities.forEach( (activity) => {
+                filterBySpreadsheetName: function (spreadsheetName) {
+                    this.visibleSpreadsheets[spreadsheetName] = !this.visibleSpreadsheets[spreadsheetName];
+                    this.sheetsData.forEach( (item) => {
+                        item.activities.forEach( (activity) => {
                           if(activity.spreadsheetName === spreadsheetName) {
-                              activity.isVisible = !activity.isVisible;
+                              activity.isVisible = this.visibleSpreadsheets[spreadsheetName];
                           }
-                      });
+                        });
+                        
+                        this.visibleSpreadsheetsPerDay[spreadsheetName][item.day] = this.visibleSpreadsheets[spreadsheetName];
                   });
-                }
+                },
           },
-      
       });
-      
     }
-    
 }
-
-
