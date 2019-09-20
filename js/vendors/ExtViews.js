4909797fbd26a66d5c36948e2611ab27db0dbf5b
wlodi <wlodek.grzegorz@gmail.com>
2019-09-21 00:23:29 +0200

    First mobile version plus refacor

--- js/vendors/ExtViews.js
+++ js/vendors/ExtViews.js
@@ -27,10 +27,11 @@ class ExtViews {
                 closeAnimationDuration: 0
             });
             viewer.on('open', () => {
-                WebUtil.waitForDomElement('#ext-views-wrapper > div.ext-views-content', 40, (element) => {
+                WebUtil.waitForDomElement('#ext-views-wrapper > div.ext-views-content', 40, (imageContainer) => {
                     ExtViews.snapBrowserScroll(img);
-                    ExtViews.createDescription(triggers[index]);
-                    ExtViews.createNavigation(viewer, element, triggers, index);
+                    ExtViews.createLabel(triggers[index]);
+                    ExtViews.createMealDescription(triggers[index], imageContainer);
+                    ExtViews.createNavigation(viewer, imageContainer, triggers, index);
                 });
             });
         });
@@ -43,7 +44,7 @@ class ExtViews {
         }
     }
 
-    static createDescription(sourceImageDom) {
+    static createLabel(sourceImageDom) {
         if (typeof sourceImageDom === "undefined" || sourceImageDom == null) {
             return;
         }
@@ -53,15 +54,37 @@ class ExtViews {
             return;
         }
 
-        imageContainer.setAttribute("data-image-description", sourceImageDom.firstChild.getAttribute("data-image-description"));
+        imageContainer.setAttribute("data-image-label", sourceImageDom.firstChild.getAttribute("data-image-label"));
     }
 
-    static createNavigation(viewer, element, triggers, index) {
+    // TODO dodac rozwijany opis do zdjec
+    static createMealDescription(sourceImageDom, imageContainer) {
+        if (typeof sourceImageDom === "undefined" || sourceImageDom == null) {
+            return;
+        }
+
+        const mealDescription = sourceImageDom.closest("div.item-content").querySelector('p[data-activity-key="meal"] > span:last-child');
+        if (typeof mealDescription === "undefined" || mealDescription == null) {
+            return;
+        }
+
+        console.log("Description: ", mealDescription.innerHTML);
+
+        const container = document.createElement("div");
+        container.setAttribute("class", "ext-views-meal-description");
+
+        const textNodeLabel = document.createTextNode("Recipe");
+        container.appendChild(textNodeLabel);
+
+        imageContainer.appendChild(container);
+    }
+
+    static createNavigation(viewer, imageContainer, triggers, index) {
         const nextImageDom = triggers[index + 1];
         const prevImageDom = triggers[index - 1];
 
-        const prevNavigation = ExtViews.createNavigationDOMElement(viewer, "Prev (arrow left)", element, prevImageDom, "ext-views-navigation-prev");
-        const nextNavigation = ExtViews.createNavigationDOMElement(viewer, "Next (arrow right)", element, nextImageDom, "ext-views-navigation-next");
+        const prevNavigation = ExtViews.createNavigationDOMElement(viewer, "Prev (arrow left)", imageContainer, prevImageDom, "ext-views-navigation-prev");
+        const nextNavigation = ExtViews.createNavigationDOMElement(viewer, "Next (arrow right)", imageContainer, nextImageDom, "ext-views-navigation-next");
 
         // TODO for now "document.onkeydown" is only used in this place
         document.onkeydown = function (e) {
@@ -74,7 +97,7 @@ class ExtViews {
         }
     }
 
-    static createNavigationDOMElement(viewer, label, parent, imageDom, clazz) {
+    static createNavigationDOMElement(viewer, label, imageContainer, imageDom, clazz) {
         if (typeof imageDom !== "undefined" && imageDom !== null) {
             const container = document.createElement("div");
             container.setAttribute("class", "ext-views-navigation " + clazz);
@@ -88,7 +111,7 @@ class ExtViews {
             textNodeLabelContainer.append(textNodeLabel);
             container.appendChild(textNodeLabelContainer);
 
-            parent.appendChild(container);
+            imageContainer.appendChild(container);
 
             return container;
         }
